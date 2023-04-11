fe5d745cecd9b3a552bfd71a5168d148d5ff45dc
diep <nvdiep11062000@gmail.com>
2023-04-11 10:07:20 +0700

    update

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -8,6 +8,7 @@ const Contact = () => {
             <div className="contact">
                 <div className="container">
                     <iframe
+                        title="This is a unique title"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2450.0960068130544!2d105.78394800822988!3d21.017003584218315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab54df748467%3A0xbce19e4662752253!2sInterContinental%20Hanoi%20Landmark!5e0!3m2!1svi!2s!4v1678328122497!5m2!1svi!2s"
                         width="100%"
                         height="400"
